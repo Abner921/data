ba90cabcd069595870c25fd9b469576793425e54
happyili <happyili@hotmail.com>
2014-06-26 14:53:55 +0800

    - refactor the single action processor out for reuse without operations - add the meihua data xml parser

---
+++ py_src/MeihuaDataParser.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import os,sys,time
+from datetime import date, timedelta
+from fetcher.data_fetcher import DataFetcher
+from fetcher.utility import Utility
+import MySQLdb
+import importlib
+from xml.dom import minidom
+
+"""
+<MeihuaNet>
+<Count>8</Count>
+<PageCount>1</PageCount>
+<Last4MonthCount>1145</Last4MonthCount>
+<Item>
+<Repeat>0</Repeat>
+<ID>10073935</ID>
+<HeaderLabel>让更多上海人住进万科装修房</HeaderLabel>
+<AdvertiserID>224821</AdvertiserID>
+<AdvertiserName>昆山万科房地产有限公司</AdvertiserName>
+<BrandCode>254991</BrandCode>
+<BrandName>上海魅力之城</BrandName>
+<BrandNameEN/>
+<MediumID>419</MediumID>
+<MediumName>I时代报</MediumName>
+<MediumNameEN>METRO EXPRESS</MediumNameEN>
+<CategoryCode>130102</CategoryCode>
+<CategoryName>住宅-公寓</CategoryName>
+<Celebrities/>
+<ReleaseDate>2014-06-19</ReleaseDate>
+<PageNumber>第封一版</PageNumber>
+<LayoutStyle>半版</LayoutStyle>
+<PicHeight>11.51</PicHeight>
+<PicWidth>23.09</PicWidth>
+<ProductModel>公寓</ProductModel>
+<AdExpense>4.48</AdExpense>
+<SuppleForceType>硬广告</SuppleForceType>
+<GraphicSmall>
+http://adm.meihua.info/PicLoader.aspx?type=0&p=sBoehHjcSW6FUxXzUcThFgx6V7rXvsBVoyz0rbAm7b7yCLeC7YU6Hrei0rgejhGsmLqtVT2Ji%2bdd9b9x4wVbHA%3d%3d&fn=picture&t=0
+</GraphicSmall>
+<GraphicLarge>
+http://adm.meihua.info/PicLoader.aspx?type=0&p=sBoehHjcSW6FUxXzUcThFgx6V7rXvsBVoyz0rbAm7b6RkGaWJA36mv93X5tDK7rJwNoCYbKFu38%3d&fn=picture&t=0
+</GraphicLarge>
+<ColorType>彩色</ColorType>
+<PageSize>0.4</PageSize>
+<PaperType>新闻</PaperType>
+<AdObject>产品宣传</AdObject>
+<MediumCity>上海市</MediumCity>
+<Browse>1】</Browse>
+</Item>
+"""
+
+class MeihuaDataParser:
+
+  def parseData(self, xml_content):
+    all_items = []
+    dom = minidom.parse(xml_content)
+    for node in dom.getElementsByTagName('Item'):
+      # get all key value pair
+      one_item = {}
+      # for each child there should be only one sub text child.
+      for child in node.childNodes:
+        if (len(child.childNodes) > 0):
+          subchild = child.childNodes[0]
+          text = subchild.nodeValue
+          one_item[child.nodeName] = text          
+      all_items.append(one_item)
+  
+    return all_items
+  
--- py_src/SendRequest.py
+++ py_src/SendRequest.py
@@ -8,235 +8,12 @@ import socket
 from urllib2 import Request, urlopen, URLError, HTTPError
 from Utility import Utility
 from RequestInfoLoader import *
+from SingleActionProcessor import *
 from SiteData import *
 from ErrorCode import *
 
-COOKIES_FILE = './kan_cookies.dat'
-TIMEOUTS = 50
-
-cj = None
-opener = None
-utility = Utility()
 request_info_loader = RequestInfoLoader(utility)
-
-socket.setdefaulttimeout(TIMEOUTS)
-
-# Note: Don't install_opener as a global opener for now, considering
-# timeouts. For each batch-send/fetch, we require one explicit login.
-# Also, we don't use Basic Auth Handler.
-cj = cookielib.CookieJar()
-opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-urllib2.install_opener(opener)
-
-def getEncodedUrl(actionInfo):
-  if actionInfo.has_key("url_params") and actionInfo["url_params"]:
-    return actionInfo["url"] + "?" + urllib.urlencode(actionInfo["url_params"])
-  else:
-    return actionInfo["url"]
-
-
-def printActionRequestDebugInfo(shouldPrint, actionInfo, inputInfo, request):
-  """ Only print, don't change the request / response
-  """
-  utility.printMessage("========================= Action: " + actionInfo["action_name"], shouldPrint)
-  requestUrl = getEncodedUrl(actionInfo)
-  payLoadData = getPayLoadData(actionInfo)
-  utility.printMessage("========================= Request: " + requestUrl, shouldPrint)
-  utility.printDebug(request, shouldPrint)
-  utility.printDetails(payLoadData, shouldPrint)
-
-
-def printActionResponseDebugInfo(shouldPrint, actionInfo, response, content):
-  requestUrl = getEncodedUrl(actionInfo)
-  utility.printMessage("========================= Response: " + requestUrl , shouldPrint)
-  utility.printDebug(response, shouldPrint)
-  utility.printMessage("========================= Response header", shouldPrint)
-  utility.printDebug(response.headers, shouldPrint)
-  utility.printMessage("", shouldPrint)
-  utility.printMessage("========================= ResponseContent: " + requestUrl, shouldPrint)
-  utility.printDetails(content, shouldPrint)
-  utility.outputCookie(cj, "", shouldPrint)
-
-
-def getPayLoadData(actionInfo):
-  payLoadData = None
-  if actionInfo.has_key("data") and actionInfo["data"]:
-    if type(actionInfo["data"]) is str:
-      payLoadData = actionInfo["data"]
-    else:
-      payLoadData = urllib.urlencode(actionInfo["data"])
-
-  return payLoadData
-
-def processActionError(actionInfo, content):
-  if actionInfo.has_key("error_handling"):
-    for errorInfo in actionInfo["error_handling"]:
-      # print "error_regex: ", errorInfo["error_regex"]
-      error_regex = getRegexMatcher(errorInfo, errorInfo["error_regex"])
-      match_errors = error_regex.findall(content)
-      if match_errors:
-        if errorInfo.has_key("error_message"):
-          error_message = errorInfo["error_message"]
-        else:
-          error_message = match_errors[0]
-
-        if shouldRetry(errorInfo["error_code"]):
-          error_message += "  Should retry."
-
-        utility.printError(
-            message = actionInfo["action_name"] + " error: " + error_message,
-            isUserFacingError = True)
-        return errorInfo["error_code"]
-
-  return ErrorCode.ACTION_SUCCEED
-
-def processActionResult(actionInfo, response, content, inputInfo, shouldPrint):
-  if not actionInfo.has_key("result"):
-    #utility.printError("No result regex defined.")
-    return processActionError(actionInfo, content)
-
-  resultActionInfos = actionInfo["result"]
-  for resultActionInfo in resultActionInfos:
-    matchResult = matchActionResult(resultActionInfo, response, content, shouldPrint)
-    if matchResult is None:
-
-      actionError = processActionError(actionInfo, content)
-      if actionError != ErrorCode.ACTION_SUCCEED:
-        return actionError
-      else:
-        # If no error matched, or no error handling, it still doesn't match expected result.
-        utility.printError(
-            message = actionInfo["action_name"] + " error: No result found.",
-            isUserFacingError = True)
-        return ErrorCode.ACTION_NO_RESULT
-
-    else:
-      if resultActionInfo.has_key("output_value"):
-        inputInfo[resultActionInfo["output_key"]] = resultActionInfo["output_value"]
-      else:
-        inputInfo[resultActionInfo["output_key"]] = matchResult
-      utility.printMessage(
-          "SUCCEED: " + actionInfo["action_name"] + " Result: " + inputInfo[resultActionInfo["output_key"]])
-
-  return ErrorCode.ACTION_SUCCEED
-
-def getRegexMatcher(regMatchInfo, regPattern):
-  """
-  
-  Note:
-  re.S:.match any char including newline
-  re.M:^$ match each line instead of the first line.
-  >>> re.findall(r"^a(\d+)b", "a23b\na34b", re.M)
-          ['23', '34']
-  """
-  caseSensitive = True
-  matchMultiline = False
-  if regMatchInfo.has_key("case_sensitive"):
-    caseSensitive = regMatchInfo["case_sensitive"]
-  if regMatchInfo.has_key("match_multiline"):
-    matchMultiline = regMatchInfo["match_multiline"]
-  regex_flag = 0
-  if not caseSensitive:
-    regex_flag = re.IGNORECASE
-  if matchMultiline:
-    regex_flag = regex_flag | re.S
-
-  return re.compile(regPattern, regex_flag)
-
-def matchActionResult(resultActionInfo, response, content, shouldPrint):
-  checkRedirectUrl = resultActionInfo.has_key("response_regex")
-  allMatches = None
-  if checkRedirectUrl:
-    # for the case when it is a 302 with redirect url when succeed
-    redirect_regex = getRegexMatcher(resultActionInfo, resultActionInfo["response_regex"])
-    responseStringIO = StringIO.StringIO()
-    pprint(vars(response), responseStringIO)
-    responseStringIO.write("\n")
-    pprint(vars(response.headers), responseStringIO)
-    responseObjectContent = responseStringIO.getvalue() + "\r\n" + utility.getCookieValues(cj)
-    print "Response: ", responseObjectContent
-    allMatches = redirect_regex.findall(responseObjectContent)
-  else:
-    result_regex = getRegexMatcher(resultActionInfo, resultActionInfo["result_regex"])
-    allMatches = result_regex.findall(content)
-
-  if len(allMatches) < 1:
-    if checkRedirectUrl:
-      utility.printError("Response has no matches for response regex: "
-                         + resultActionInfo["response_regex"])
-    else:
-      utility.printError("Response has no matches for result regex: "
-                         + resultActionInfo["result_regex"])
-    #utility.printMessage(content, shouldPrint)
-    return None
-  else:
-    if len(allMatches) > 1:
-      utility.printMessage("Response has multiple matches for result. Getting the first one: " + allMatches[0])
-      if checkRedirectUrl:
-        utility.printMessage("Response Regex:" + resultActionInfo["response_regex"], shouldPrint)
-      else:
-        utility.printMessage("Result Regex:" + resultActionInfo["result_regex"])
-
-    return allMatches[0]
-
-def processOneAction(actionOriginInfo, inputInfo, shouldPrint):
-  actionInfo = copy.deepcopy(actionOriginInfo)
-  utility.processSiteData(actionInfo, inputInfo)
-
-  utility.printMessage("Start: " + actionInfo["action_name"])
-  if actionInfo.has_key("type") and actionInfo["type"] == "ADD_IMAGE":
-    if not inputInfo.has_key("PRODUCT_IMAGE_FILE_PATH") or not inputInfo["PRODUCT_IMAGE_FILE_PATH"]:
-      utility.printMessage("Skip image request as no image is uploaded")
-      return ErrorCode.ACTION_SUCCEED
-
-  requestUrl = getEncodedUrl(actionInfo)
-  payLoadData = getPayLoadData(actionInfo)
-  request = urllib2.Request(
-      url = requestUrl,
-      headers = actionInfo["headers"],
-      data = payLoadData)
-  printActionRequestDebugInfo(shouldPrint, actionInfo, inputInfo, request)
-
-  try:
-    response = urllib2.urlopen(request)
-    if response.info().get('Content-Encoding') and response.info().get('Content-Encoding').find('gzip') >= 0:
-      utility.printMessage('gzip enabled: suggest to remove the Content-Encoding setting!')
-      buf = StringIO.StringIO(response.read())
-      gzipFile = gzip.GzipFile(fileobj=buf)
-      content = gzipFile.read()
-    else:
-      content = response.read()
-
-    printActionResponseDebugInfo(shouldPrint, actionInfo, response, content)
-    return processActionResult(actionInfo, response, content, inputInfo, shouldPrint)
-
-  except HTTPError, e:
-    utility.printError('The server couldn\'t fulfill the request. Error code: ' + str(e.code))
-    if actionInfo.has_key('error_strategy'):
-      if actionInfo['error_strategy'] == "ALLOW_ERROR":
-        return ErrorCode.ACTION_SUCCEED
-    return ErrorCode.ACTION_RETRY_HTTP_ERROR
-  except URLError, e:
-    utility.printError('We failed to reach a server. Reason: ' + str(e.reason))
-    return ErrorCode.ACTION_RETRY_URL_ERROR
-  except socket.timeout, e:
-    errno, errstr = sys.exc_info()[:2]
-    utility.printError("Socket Timeout (1): " + errno + ": " + errstr)
-    return ErrorCode.ACTION_RETRY_TIMEOUT
-  except socket.error, e:
-    errno, errstr = sys.exc_info()[:2]
-    if errno == socket.timeout:
-      utility.printError("Socket Timeout (2): " + errno + ": " + errstr)
-      return ErrorCode.ACTION_RETRY_TIMEOUT
-    else:
-      utility.printError("Socket Error: " + errno + ": " + errstr)
-      return ErrorCode.ACTION_SOCKET_ERROR
-
-def shouldRetry(returnCode):
-  return (returnCode == ErrorCode.ACTION_RETRY_NO_RESULT or
-          returnCode == ErrorCode.ACTION_RETRY_TIMEOUT or
-          returnCode == ErrorCode.ACTION_RETRY_HTTP_ERROR or
-          returnCode == ErrorCode.ACTION_RETRY_URL_ERROR)
+actionProcessor = SingleActionProcessor()
 
 def processOneOperation(inputObjectInfo, siteInfo, operations):
   returnCode = None
@@ -257,13 +34,13 @@ def processOneOperation(inputObjectInfo, siteInfo, operations):
       utility.printMessage("-------------------")
 
       retryTimes = 3
-      returnCode = processOneAction(actionInfo, inputInfo, inputInfo.has_key("DEBUG"))
+      returnCode = actionProcessor.processOneAction(actionInfo, inputInfo, inputInfo.has_key("DEBUG"))
 
       backupData = backupFallbackDataForAction(inputInfo, actionInfo)
       # Give it a couple retries for timeout or no-result-fallback.
       # Should only rety for timeout, or unknown reason, or explicitly retry request.
       # For login failure and other cases we should just fail fast.
-      while (shouldRetry(returnCode) and retryTimes > 0):
+      while (actionProcessor.shouldRetry(returnCode) and retryTimes > 0):
         utility.printMessage("Retry for error: " + str(returnCode))
 
         # use the fallback data in input in case there is no result.
@@ -271,7 +48,7 @@ def processOneOperation(inputObjectInfo, siteInfo, operations):
           fillFallbackDataForAction(inputInfo, actionInfo)
 
         # else: no change on input and just retry for time out issues.
-        returnCode = processOneAction(actionInfo, inputInfo, inputInfo.has_key("DEBUG"))
+        returnCode = actionProcessor.processOneAction(actionInfo, inputInfo, inputInfo.has_key("DEBUG"))
         retryTimes = retryTimes - 1
 
       if returnCode != ErrorCode.ACTION_SUCCEED:
