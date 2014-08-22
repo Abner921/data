0af23a142755538c0cb4af4a0c245646f653412b
shuxue051 <shuxue051@gmail.com>
2014-08-22 15:22:43 +0800

    fix set -e

--- baidu_ads_api_fetcher/sem/reportPipeline.sh
+++ baidu_ads_api_fetcher/sem/reportPipeline.sh
@@ -1,34 +1,34 @@
 #!/bin/sh
-set -e
+
 basepath=$(cd `dirname $0`; pwd -P)
 cd $basepath
 echo $basepath
 logpath=log/`date +%Y-%m/%d`
 mkdir -p $logpath
-
+set -e
 #Keyword
-echo "start ferch Keyword report"
+echo "start fetch Keyword report"
 filename=sem-baidu-Keyword.log
-echo "start ferch Keyword report" >> $logpath/$filename
+echo "start fetch Keyword report" >> $logpath/$filename
 python ReportPipeline.py -r Keyword -d pc -u day >> $logpath/$filename  2>&1
 python ReportPipeline.py -r Keyword -d mobile -u day >> $logpath/$filename  2>&1
-echo "end ferch Keyword report" >> $logpath/$filename
+echo "end fetch Keyword report" >> $logpath/$filename
 
 #Campaign
-echo "start ferch Campaign report"
+echo "start fetch Campaign report"
 filename=sem-baidu-Campaign.log
-echo "start ferch Campaign report" >> $logpath/$filename
+echo "start fetch Campaign report" >> $logpath/$filename
 python ReportPipeline.py -r Campaign -d pc -u hour >> $logpath/$filename  2>&1
 python ReportPipeline.py -r Campaign -d mobile -u hour >> $logpath/$filename  2>&1
-echo "end ferch Campaign report" >> $logpath/$filename
+echo "end fetch Campaign report" >> $logpath/$filename
 
 #Region
-echo "start ferch Region report"
+echo "start fetch Region report"
 filename=sem-baidu-Region.log
-echo "start ferch Region report" >> $logpath/$filename
+echo "start fetch Region report" >> $logpath/$filename
 python ReportPipeline.py -r Region -d pc -u day >> $logpath/$filename  2>&1
 python ReportPipeline.py -r Region -d mobile -u day >> $logpath/$filename  2>&1
-echo "end ferch Region report" >> $logpath/$filename
+echo "end fetch Region report" >> $logpath/$filename
 
 
 
