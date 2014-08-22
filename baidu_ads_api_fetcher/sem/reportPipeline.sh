819ffe08b6389766dc0d0503254ea7c867761132
shuxue051 <shuxue051@gmail.com>
2014-08-22 15:18:53 +0800

    fix logpath

--- baidu_ads_api_fetcher/sem/reportPipeline.sh
+++ baidu_ads_api_fetcher/sem/reportPipeline.sh
@@ -1,29 +1,34 @@
 #!/bin/sh
+set -e
 basepath=$(cd `dirname $0`; pwd -P)
 cd $basepath
 echo $basepath
-mkdir -p log/`date +%Y-%m/%d`
+logpath=log/`date +%Y-%m/%d`
+mkdir -p $logpath
 
 #Keyword
 echo "start ferch Keyword report"
-echo "start ferch Keyword report" >> log/`date +%Y-%m/%d`/sem-baidu-Keyword.log
-python ReportPipeline.py -r Keyword -d pc -u day >> log/`date +%Y-%m/%d`/sem-baidu-Keyword.log  2>&1
-python ReportPipeline.py -r Keyword -d mobile -u day >> log/`date +%Y-%m/%d`/sem-baidu-Keyword.log  2>&1
-echo "end ferch Keyword report" >> log/`date +%Y-%m/%d`/sem-baidu-Keyword.log
+filename=sem-baidu-Keyword.log
+echo "start ferch Keyword report" >> $logpath/$filename
+python ReportPipeline.py -r Keyword -d pc -u day >> $logpath/$filename  2>&1
+python ReportPipeline.py -r Keyword -d mobile -u day >> $logpath/$filename  2>&1
+echo "end ferch Keyword report" >> $logpath/$filename
 
 #Campaign
 echo "start ferch Campaign report"
-echo "start ferch Campaign report" >> log/`date +%Y-%m/%d`/sem-baidu-Campaign.log
-python ReportPipeline.py -r Campaign -d pc -u hour >> log/`date +%Y-%m/%d`/sem-baidu-Campaign.log  2>&1
-python ReportPipeline.py -r Campaign -d mobile -u hour >> log/`date +%Y-%m/%d`/sem-baidu-Campaign.log  2>&1
-echo "end ferch Campaign report" >> log/`date +%Y-%m/%d`/sem-baidu-Campaign.log
+filename=sem-baidu-Campaign.log
+echo "start ferch Campaign report" >> $logpath/$filename
+python ReportPipeline.py -r Campaign -d pc -u hour >> $logpath/$filename  2>&1
+python ReportPipeline.py -r Campaign -d mobile -u hour >> $logpath/$filename  2>&1
+echo "end ferch Campaign report" >> $logpath/$filename
 
 #Region
 echo "start ferch Region report"
-echo "start ferch Region report" >> log/`date +%Y-%m/%d`/sem-baidu-Region.log
-python ReportPipeline.py -r Region -d pc -u day >> log/`date +%Y-%m/%d`/sem-baidu-Region.log  2>&1
-python ReportPipeline.py -r Region -d mobile -u day >> log/`date +%Y-%m/%d`/sem-baidu-Region.log  2>&1
-echo "end ferch Region report" >> log/`date +%Y-%m/%d`/sem-baidu-Region.log
+filename=sem-baidu-Region.log
+echo "start ferch Region report" >> $logpath/$filename
+python ReportPipeline.py -r Region -d pc -u day >> $logpath/$filename  2>&1
+python ReportPipeline.py -r Region -d mobile -u day >> $logpath/$filename  2>&1
+echo "end ferch Region report" >> $logpath/$filename
 
 
 
